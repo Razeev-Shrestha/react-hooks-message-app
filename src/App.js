400381984135d253d6671e5601d9d7abc8b9226f
Razeev-stha <yuvrajstha1246@outlook.com>
2021-05-09 13:44:43 +0545

    added log and removed console

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ const App = () => {
 	useEffect(() => {
 		pubsub.addListener({
 			message: (messageObject) => {
-				const { channel, message } = messageObject
-				console.log('Received message', message, 'channel', channel)
+				const { message } = messageObject
 
 				dispatch(message)
 			},
